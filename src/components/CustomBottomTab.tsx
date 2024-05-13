455633d95bd23c1f0a2485224d01db40d3b90ad0
Luan Vinícius <luanviniciuzz@gmail.com>
2024-05-13 15:32:40 -0300

    animation tab bar

--- src/components/CustomBottomTab.tsx
+++ src/components/CustomBottomTab.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {StyleSheet, View, Text, Pressable, useWindowDimensions} from 'react-native';
 import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { useAnimatedStyle,withSpring } from "react-native-reanimated";
+import Animated, { useAnimatedStyle,withSpring, withTiming } from "react-native-reanimated";
 import BottomTabIcon from './BottomTabIcon';
 
 const CustomBottomTab = ({state, descriptors, navigation}: BottomTabBarProps) => {
@@ -15,58 +15,69 @@ const CustomBottomTab = ({state, descriptors, navigation}: BottomTabBarProps) =>
 
   const translateAnimation = useAnimatedStyle(() => {
     return {
-      transform: [{translateX: withSpring(TAB_WIDTH * state.index)}],
+      transform: [{translateX: withTiming(TAB_WIDTH * state.index)}],
     };
   }); 
   return(
     <View style={[styles.tabBarContainer, {width: TAB_BAR_WIDTH, bottom:MARGIN}]}>
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
+      <Animated.View style={[
+        styles.slidingTabContainer,
+        {width:TAB_WIDTH},
+        translateAnimation
+        ]}>
+        <View style={styles.slidingTab}/>
+      </Animated.View>
+      {
+          state.routes.map((route, index) => {
+            const { options } = descriptors[route.key];
 
-        const isFocused = state.index === index;
+            const isFocused = state.index === index;
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
+            const onPress = () => {
+              const event = navigation.emit({
+                type: 'tabPress',
+                target: route.key,
+                canPreventDefault: true,
+              });
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name, {merge:true});
-          }
-        };
+              if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name, {merge:true});
+            }
+          };
 
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
 
-        return (
-          <Pressable
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={{ flex: 1 }}
-          >
-            <View style={styles.contentContainer}>
-              <BottomTabIcon route={route.name} isFocused={isFocused} />
-              <Text
-                style={{
-                  color: isFocused ? '#0067FF' : 'white',
-                  fontSize: 12,
-                }}>
-                {route.name}
-              </Text>
-            </View>
-          </Pressable>
-        );
-      })}
+          return (
+            <Pressable
+              key={index}
+              accessibilityRole="button"
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={{ flex: 1 }}
+            >
+              <View style={styles.contentContainer}>
+                <BottomTabIcon route={route.name} isFocused={isFocused} />
+                {isFocused ? <Text
+                  style={{
+                    color: isFocused ? '#0067FF' : 'white',
+                    fontSize: 12,
+                  }}>
+                  {route.name}
+                </Text> : null }
+                
+              </View>
+            </Pressable>
+          );
+        })
+      }
     </View>
   )
 }
