d173cb95f433480ffe0350e830e80999f08ce03e
Luan Vinícius <luanviniciuzz@gmail.com>
2024-05-14 01:46:05 -0300

    icon and splash animation

--- src/Splash.tsx
+++ src/Splash.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import {Image, Text, View} from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import {Image, Text, View, Animated} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from './routes';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import THEME from './assets/styles/theme'
+import { useSharedValue, withTiming } from 'react-native-reanimated';
 import Container from './components/Container';
+
+const FadeInView = (props: any) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 2000, // Ajuste a duração conforme necessário
+        useNativeDriver: true
+      }
+    ).start();
+  }, [fadeAnim]);
+
+  return (
+    <Animated.View
+      style={{
+        ...props.style,
+        opacity: fadeAnim,
+      }}
+    >
+      {props.children}
+    </Animated.View>
+  );
+}
 
 const Splash = () => {
 
@@ -17,14 +44,23 @@ const Splash = () => {
   React.useEffect(() => {
       setTimeout(() => {
         star()
-      },1500)
+      },2500)
   },[])
 
   return (
     <Container>
-      <Image
-        source={require('./assets/img/logo.png')}
-      />
+      <FadeInView>
+        <Image
+          source={require('./assets/img/getup.png')}
+          style={{width:150, height:150}}
+        />
+      </FadeInView>
+      <FadeInView>
+        <Image
+          source={require('./assets/img/text.png')}
+          
+        />
+      </FadeInView>
     </Container>
   );
 };
