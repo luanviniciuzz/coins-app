5fce24a294a2fbb9af675a44f7cd4ea5ff747fa5
Luan Vinícius <luanviniciuzz@gmail.com>
2024-05-17 00:17:08 -0300

    home screen and grph

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,54 +1,138 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Text, View, FlatList } from 'react-native';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
+import { Text,StyleSheet, View, Dimensions, ScrollView } from 'react-native';
 import Container from '../../components/Container';
-
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import THEME from '../../assets/styles/theme';
+//import LineChart from '../../components/LineChart';
+import { CandlestickChart, LineChart } from 'react-native-wagmi-charts';
+import FormatCoin from '../../utils/FormatCoin';
 
 const HomePage = () => {
-  const ws = useRef<WebSocket | undefined>(undefined);
-  const [price, setPrice] = useState<number>(0.0)
-  const [valor, setValor] = useState<number>(0.0)
-  const convert = (eth:number) =>{
-    //1 Ethereum equals R$14,890.58 Real
-    let formula = (14890.58 * eth)
-
-    setValor(formula)
+  const [price, setPrice] = useState<string>('')
+  
+  const MAX_LINE_INDEX = 30
+  const WIDTH = Dimensions.get('screen').width * 0.90
+  const HEIGHT = Dimensions.get('screen').height * 0.30
+  const [lineData, setLineData] =  useState<any[]>([]);
+  const [candleData, setCandleData] = useState<any[]>([]);
+  
+  async function callWebSocketIndex(){
+    const ws = new WebSocket('wss://nbstream.binance.com/eoptions/ws/ETHUSDT@index')
+    ws.onopen
+    ws.onmessage = (event) => {
+      let result = JSON.parse(event.data)
+      const formatValue = FormatCoin(result.p)
+      setPrice(formatValue)
+      const point = {
+        timestamp: result.E,
+        value: result.p,
+      };
+      setLineData((prev) => {
+        const newData = [...prev, point];
+        if (newData.length > 30) {
+          return newData.slice(newData.length - 30);
+        }
+        return newData;
+      });
+    };
+    ws.onclose
+    return () => {
+      ws.close();
+    };
   }
-  ///DESCOMENTA WEBSOCKET
-  // useEffect(() => {
-  //   const ws = new WebSocket('wss://nbstream.binance.com/eoptions/ws/ETHUSDT@index');
 
-  //   ws.onopen = () => {
-  //     console.log('WebSocket connection established.');
-  //   };
+  async function callWebSocketKLine(){
 
-  //   ws.onmessage = (event) => {
-  //     let result = JSON.parse(event.data)
-  //     setPrice(result.p)
-  //     convert(result.p)
-  //     console.log('Received message:', result);
-  //     // Aqui você pode manipular a mensagem recebida
-  //   };
+    const ws = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@kline_1d');
+    ws.onopen
+    ws.onmessage = (event) => {
+      let result = JSON.parse(event.data)
+      const point = {
+        timestamp: result.E,
+        open: result.k.o,
+        high: result.k.h,
+        low: result.k.l,
+        close: result.k.c,
+      };
+      setCandleData((prev) => {
+        const newData = [...prev, point];
+        if (newData.length > 30) {
+          return newData.slice(newData.length - 30);
+        }
+        return newData;
+      });
+    };
+    ws.onclose
+    return () => {
+      ws.close();
+    };
+  }
 
-  //   ws.onclose = () => {
-  //     console.log('WebSocket connection closed.');
-  //   };
+  useEffect(() => {
+    async function index(){
+      await callWebSocketIndex()
+    }
+    index()
+  }, []);
 
-  //   // Cleanup function para fechar a conexão WebSocket
-  //   return () => {
-  //     ws.close();
-  //   };
-  // }, []);
+  useEffect(() => {
+    async function index(){
+      await callWebSocketKLine()
+    }
+    index()
+  }, []);
 
+
+   const MAX_POINTS = 30;
+   const CANDLE_SIZE = 15;
+   const CHART_WIDTH = Dimensions.get("window").width - 35;
+   const CHART_HEIGHT = 200;
   return (
-    <Container>
-
-      <Text style={{ color: 'white' }}>{price}</Text>
-      {/* <Text style={{ color: 'white' }}>{valor}</Text> */}
+    <ScrollView style={{
+      flex: 1,
+      padding:'5%',
+      backgroundColor:THEME.COLORS.BACKGROUND
+    }}>
+      <View style ={{width:'90%', height:90}}>
+        <Text style={{ color: THEME.COLORS.WHITE , fontSize:40, }}>{'Ethereum'}</Text>
+        <Text style={{ color: THEME.COLORS.WHITE, fontSize:25 }}>{price}</Text>
+      </View>
+      <GestureHandlerRootView>
+            {lineData.length >= 5 ? (
+              <LineChart.Provider data={lineData}>
+                <LineChart width={CHART_WIDTH} height={CHART_HEIGHT}>
+                  <LineChart.Path color={THEME.COLORS.PURPLE}>
+                    <LineChart.Gradient color={THEME.COLORS.PURPLE}/>
+                  </LineChart.Path>
+                </LineChart>
+              </LineChart.Provider>
+            ): null}
+          {candleData.length >= 5 ? (
+            <CandlestickChart.Provider data={candleData}>
+              <CandlestickChart width={CHART_WIDTH} height={300}>
+                <CandlestickChart.Candles candleProps={{
+                    width: CANDLE_SIZE,
+                    positiveColor: THEME.COLORS.GREEN,
+                    negativeColor: THEME.COLORS.RED
+                   }}/>
+                <CandlestickChart.Crosshair />
+              </CandlestickChart>
+            </CandlestickChart.Provider>
+          ) :null}
+          
+    </GestureHandlerRootView>
 
       
-    </Container>
+    </ScrollView>
   );
 };
 
 export default HomePage;
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: THEME.COLORS.BACKGROUND
+  }
+})
