81ae691da4e0f90de755808c2d7ab5a9115c025d
Luan Vinícius <luanviniciuzz@gmail.com>
2024-05-17 00:18:05 -0300

    update archiv

--- src/pages/wallet/WalletPage.tsx
+++ src/pages/wallet/WalletPage.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import Container from '../../components/Container';
 
+
 const WalletPage = () => {
   return (
     <Container>
       <Text style={{color:'white'}}>Hello word, esta é a pagina WALLET</Text>
+       
     </Container>
   );
 };
